1272fb2a30700099be550b2bc867caea77e8d26f
Peter <petersporn96@gmail.com>
2025-10-26 22:52:42 -0600

    fix ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8b65254a1d1e5342"; }
+/******/ 		__webpack_require__.h = function() { return "578651b2a3abea14"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter, Poppins } from 'next/font/google'
 import { Navigation } from '@/components/Navigation'
-import '../styles/global.scss'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 const poppins = Poppins({ 
